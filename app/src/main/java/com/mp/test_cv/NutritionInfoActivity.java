cd0ca4f6058299ed59945308781fbe41be656f2d
seungjikim <43927910+seungjikim@users.noreply.github.com>
2020-05-09 21:19:50 +0900

    add three-buton functions to NutritionInfoActivity

--- app/src/main/java/com/mp/test_cv/NutritionInfoActivity.java
+++ app/src/main/java/com/mp/test_cv/NutritionInfoActivity.java
@@ -2,10 +2,20 @@ package com.mp.test_cv;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class NutritionInfoActivity extends AppCompatActivity {
     final String TAG = getClass().getSimpleName();
@@ -14,15 +24,57 @@ public class NutritionInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutritioninfo);
-        Button btnScan = (Button) findViewById(R.id.scan);
 
-        btnScan.setOnClickListener(new Button.OnClickListener() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        if(user == null) {
+        myStartActivity(SignUpActivity.class);
+        } else {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("User").document(user.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CameraView.class);
-                startActivity(intent);
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null) {
+                        if (document.exists()) {
+                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        } else {
+                            Log.d(TAG, "No such document");
+                            myStartActivity(MemberInitActivity.class);
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
             }
         });
+    }
+}
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.scan:
+                    myStartActivity(CameraView.class);
+                    break;
+                case R.id.calculator:
+                    myStartActivity(NutritionInfoActivity.class);
+                case R.id.submit:
+                    //사용자 정보 db 받고, 갱신
+                    //모든 정보 갱신 과정이 끝나면 mainActivity로 간다.
+                    myStartActivity(MainActivity.class);
+            }
+        }
+    };
+
+    private void myStartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //로그인 한 상태에서 뒤로가기 눌렀을 때 메인액티비로 이동, 나머지 스택 없어짐.
+
+        startActivity(intent);
     }
 }
