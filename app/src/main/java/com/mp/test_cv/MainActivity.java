c1fcd738df046f5e91f32f2f0605ae1ab455d245
Jaehee <vito822@naver.com>
2020-05-21 05:29:24 +0900

    Add RecyclerView to MainActivity

--- app/src/main/java/com/mp/test_cv/MainActivity.java
+++ app/src/main/java/com/mp/test_cv/MainActivity.java
@@ -2,26 +2,20 @@ package com.mp.test_cv;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.github.mikephil.charting.charts.ScatterChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.CombinedData;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.ScatterData;
-import com.github.mikephil.charting.data.ScatterDataSet;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -37,19 +31,26 @@ import java.util.HashMap;
 
 import java.util.ArrayList;
 import java.util.Map;
-
-import com.github.mikephil.charting.charts.CombinedChart;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     String date;
+    int totCalorie;
+    int recCalorie;
     Map<String, Integer> totNutritionMap; //Nutrition과 섭취량을 매핑할 변수 생성
-    Map<String, Integer> recNutritionMap;
+    Map<String, Integer> recNutritionMap; //권장 영양소 섭취량
     MemberInfo userInfo;
     float userBMI;
     boolean isTotalLoaded;
     boolean isRecommendLoaded;
     boolean isUserLoaded;
+
+    // RecyclerView
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,7 +78,8 @@ public class MainActivity extends AppCompatActivity {
                         if (documentSnapshot.exists()) {
                             Log.d(TAG, "DocumentSnapshot data: " + documentSnapshot.getData());
                             totalDailyIntakeMapping(totalDailyIntake);
-                            textSetUpProcess();
+                            calorieTextView();
+                            setRecyclerView();
                         } else {
                             Log.d(TAG, "No such document");
                             myStartActivity(NutritionInfoActivity.class);
@@ -93,7 +95,8 @@ public class MainActivity extends AppCompatActivity {
                     if (documentSnapshot != null) {
                         if (documentSnapshot.exists()) {
                             recommendDailyIntakeMapping(recDailyIntake);
-                            textSetUpProcess();
+                            calorieTextView();
+                            setRecyclerView();
                             Log.d(TAG, "DocumentSnapshot data: " + documentSnapshot.getData());
                         } else {
                             myStartActivity(MemberInitActivity.class);
@@ -158,101 +161,27 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    // 그래프 그리기
-    private void updateChartActivity() {
-        CombinedChart chart = findViewById(R.id.carboChart);
 
-        chart.getDescription().setEnabled(false);
-        chart.setBackgroundColor(Color.WHITE);
-        chart.setDrawGridBackground(false);
-        chart.setDrawBarShadow(false);
-        chart.setHighlightFullBarEnabled(false);
-
-        // draw bars behind scatter
-        chart.setDrawOrder(new CombinedChart.DrawOrder[]{
-                CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.SCATTER
-        });
-
-
-        // 비활성화
-        chart.setTouchEnabled(false);
-        chart.setDragEnabled(false);
-        chart.setScaleEnabled(false);
-        chart.setPinchZoom(false);
-
-        // 범례
-        Legend l = chart.getLegend();
-        l.setWordWrapEnabled(true);
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
-        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-        l.setDrawInside(false);
-
-        // x축 : 영양소
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setEnabled(false);
-
-        // y축 : 섭취량
-        YAxis leftAxis = chart.getAxisLeft();
-        leftAxis.setEnabled(true);
-        leftAxis.setDrawGridLines(true);
-        leftAxis.setDrawAxisLine(true);
-
-        // TODO : min, max 세팅 영양소별로 다르게 해줘야함
-        leftAxis.setAxisMinimum(0f);
-        leftAxis.setAxisMaximum(2000f);
-
-        YAxis rightAxis = chart.getAxisRight();
-        rightAxis.setEnabled(false);
-
-
-        // 그래프 그리기
-        int realIntake = totNutritionMap.get("carbohydrate");
-        int recommendedIntake = recNutritionMap.get("carbohydrate");
-        CombinedData data = new CombinedData();
-        data.setData(generateBarData(realIntake));
-        data.setData(generateScatterData(recommendedIntake));
-
-        chart.setData(data);
-        chart.invalidate();
-    }
-
-    private void textSetUpProcess() {
-        if (!isRecommendLoaded || !isTotalLoaded) {
-            return;
-        }
-        if (recNutritionMap.get("calories") != null && recNutritionMap.get("carbohydrate") != null) {
-            int caloriePercent = totNutritionMap.get("calories") / recNutritionMap.get("calories");
-            int carbohydratePercent = totNutritionMap.get("carbohydrate") / recNutritionMap.get("carbohydrate");
-            // 칼로리 내역 표시
-            calorieTextView(caloriePercent);
-            // 탄수화물 섭취 내역
-            updateChartActivity();   // 탄수화물 섭취 현황 차트
-            carbohydrateTextView(carbohydratePercent);
-            BMIinfoSetting();
-        } else {
-            calorieTextView(0);
-            carbohydrateTextView(0);
-            startToast("사용자 정보가 없습니다.");
-            myStartActivity(MemberInitActivity.class);
-        }
-    }
-    private void calorieTextView(int caloriePercent) {
+    private void calorieTextView() {
+        int caloriePercent;
         TextView textCaloriePercent = (TextView) findViewById(R.id.textCaloriePercent);
         TextView textRealCalorie = (TextView) findViewById(R.id.textRealCalorie);
         TextView textRecommendedCalorie = (TextView) findViewById(R.id.textRecommendedCalorie);
+
+        if(recCalorie != 0){
+            caloriePercent = totCalorie/recCalorie;
+        } else {
+            caloriePercent = 0;
+        }
+
         textCaloriePercent.setText("오늘 권장칼로리의 " + caloriePercent + "%를\n섭취하셨습니다.");
-        textRealCalorie.setText("총 섭취 칼로리 : " + totNutritionMap.get("calories") + "kcal");
-        textRecommendedCalorie.setText("권장 섭취 칼로리 : " + recNutritionMap.get("calories") + "kcal");
+        textCaloriePercent.setTextColor(Color.parseColor("#673AB7"));
+        textRealCalorie.setText("총 섭취 칼로리 : " + totCalorie+ "kcal");
+        textRecommendedCalorie.setText("권장 섭취 칼로리 : " + recCalorie + "kcal");
     }
-    private void carbohydrateTextView(int carbohydratePercent) {
-        TextView textCarbo = (TextView) findViewById(R.id.textCarbo);
-        textCarbo.setText("탄수화물\n오늘 권장탄수화물의 " + carbohydratePercent
-                + "%를\n섭취하셨습니다.\n권장섭취량 : " + recNutritionMap.get("carbohydrate") +
-                "g\n실제섭취량 : " + totNutritionMap.get("carbohydrate") + "g\n");
-    }
+
     private void totalDailyIntakeMapping(TotalDailyIntake totalDailyIntake) {
-        totNutritionMap.put("calories", totalDailyIntake.gettotalCalories());
+        totCalorie = totalDailyIntake.gettotalCarbohydrate();
         totNutritionMap.put("carbohydrate", totalDailyIntake.gettotalCarbohydrate());
         totNutritionMap.put("protein", totalDailyIntake.gettotalProtein());
         totNutritionMap.put("fat", totalDailyIntake.gettotalFat());
@@ -264,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
         isTotalLoaded = true;
     }
     private void recommendDailyIntakeMapping(RecDailyIntake recDailyIntake) {
-        recNutritionMap.put("calories", recDailyIntake.getrecCalories());
+        recCalorie = recDailyIntake.getrecCalories();
         recNutritionMap.put("carbohydrate", recDailyIntake.getrecCarbohydrate());
         recNutritionMap.put("protein", recDailyIntake.getrecProtein());
         recNutritionMap.put("fat", recDailyIntake.getrecFat());
@@ -296,46 +225,24 @@ public class MainActivity extends AppCompatActivity {
             isUserLoaded = true;
         }
     }
-    // BarData : 실제 섭취량
-    private BarData generateBarData(float realIntake) {
 
-        ArrayList<BarEntry> entries = new ArrayList<>();
+    private void setRecyclerView() {
+        if (!isRecommendLoaded || !isTotalLoaded) {
+            return;
+        }
 
-        // TODO : 실제 섭취량 받아와서 넣기
-        entries.add(new BarEntry(0f, realIntake));
-
-        BarDataSet set = new BarDataSet(entries, "실제 섭취량");
-        set.setColor(getResources().getColor(R.color.colorPrimaryLight));
-        set.setValueTextColor(Color.BLACK);
-        set.setValueTextSize(10f);
-
-
-        BarData d = new BarData(set);
-        d.setBarWidth(0.3f);
-
-        return d;
-    }
-    // ScatterData : 권장 섭취량
-    private ScatterData generateScatterData(float recommendedIntake) {
-
-        ScatterData d = new ScatterData();
-
-        ArrayList<Entry> entries = new ArrayList<>();
-
-        // TODO : 권장 섭취량 받아와서 넣기
-        entries.add(new Entry(0f, recommendedIntake));
-
-
-        ScatterDataSet set = new ScatterDataSet(entries, "권장 섭취량");
-        set.setColors(getResources().getColor(R.color.colorAccent));
-        set.setScatterShapeSize(20f);
-        set.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
-        set.setDrawValues(false);
-        set.setValueTextSize(10f);
-
-        d.addDataSet(set);
-
-        return d;
+        if (recCalorie != 0) {
+            if(totNutritionMap!=null && recNutritionMap!=null){
+                recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+                recyclerView.setHasFixedSize(true);
+                layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true);
+                recyclerView.setLayoutManager(layoutManager);
+                mAdapter = new MyAdapter(totNutritionMap, recNutritionMap);
+                recyclerView.setAdapter(mAdapter);
+            }
+        } else {
+            startToast("사용자 정보가 없습니다.");
+        }
     }
 
 }
---
+++ app/src/main/java/com/mp/test_cv/myAdapter.java
@@ -0,0 +1,233 @@
+package com.mp.test_cv;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.charts.ScatterChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.ScatterData;
+import com.github.mikephil.charting.data.ScatterDataSet;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] nutritions;
+    private Map<String, Integer> totNutritionMap;
+    private Map<String, Integer> recNutritionMap;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // 리스트에 들어가는 한 요소
+        public CombinedChart chart;
+        public TextView textViewTitle;
+        public TextView textView;
+        public MyViewHolder(LinearLayout v) {
+            super(v);
+            chart = v.findViewById(R.id.nutritionChart);
+            textViewTitle = v.findViewById(R.id.nutritionTitleText);
+            textView = v.findViewById(R.id.nutritionText);
+        }
+    }
+
+    // myDataset : 각 줄 마다 보여줄 내용을 가진다
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(Map<String, Integer> totNutritionMap, Map<String, Integer> recNutritionMap) {
+        this.totNutritionMap = totNutritionMap;
+        this.recNutritionMap = recNutritionMap;
+        nutritions = totNutritionMap.keySet().toArray(new String[0]);
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // 리스트의 한 요소에 들어갈 내용
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.today_nutrition,parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int i) {
+        String nutrition = nutritions[i];
+        int totNutrition;
+        int recNutrition;
+        int ratioNutrition;
+
+        if(totNutritionMap.get(nutrition) != null){
+            totNutrition = totNutritionMap.get(nutrition);
+        }
+        else{
+            totNutrition = 0;
+        }
+
+        if(recNutritionMap.get(nutrition) != null){
+            recNutrition = recNutritionMap.get(nutrition);
+            ratioNutrition = totNutrition / recNutrition;
+        }
+        else{
+            recNutrition = 0;
+            ratioNutrition = 0;
+        }
+
+        // 텍스트
+
+        switch(nutritions[i]){
+            case "carbohydrate" :
+                nutrition = "탄수화물";
+                break;
+
+            case "protein" :
+                nutrition = "단백질";
+                break;
+
+            case "fat" :
+                nutrition = "지방";
+                break;
+
+            case "saturatedFat" :
+                nutrition = "포화지방";
+                break;
+
+            case "sugar" :
+                nutrition = "당류";
+                break;
+
+            case "sodium" :
+                nutrition = "나트륨";
+                break;
+
+            case "dietaryfiber" :
+                nutrition = "식이섬유";
+                break;
+        }
+
+        holder.textViewTitle.setText(nutrition);
+        holder.textView.setText("오늘 권장섭취량의 "
+                + ratioNutrition
+                + "%를\n섭취하셨습니다.\n권장섭취량 : " + recNutrition +
+                "g\n실제섭취량 : " + totNutrition + "g\n");
+
+        // 차트
+        holder.chart.getDescription().setEnabled(false);
+        holder.chart.setBackgroundColor(Color.WHITE);
+        holder.chart.setDrawGridBackground(false);
+        holder.chart.setDrawBarShadow(false);
+        holder.chart.setHighlightFullBarEnabled(false);
+
+        // draw bars behind scatter
+        holder.chart.setDrawOrder(new CombinedChart.DrawOrder[]{
+                CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.SCATTER
+        });
+
+
+        // 비활성화
+        holder.chart.setTouchEnabled(false);
+        holder.chart.setDragEnabled(false);
+        holder.chart.setScaleEnabled(false);
+        holder.chart.setPinchZoom(false);
+
+        // 범례
+        Legend l = holder.chart.getLegend();
+        l.setWordWrapEnabled(true);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+
+        // x축 : 영양소
+        XAxis xAxis = holder.chart.getXAxis();
+        xAxis.setEnabled(false);
+
+        // y축 : 섭취량
+        YAxis leftAxis = holder.chart.getAxisLeft();
+        leftAxis.setEnabled(true);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setDrawAxisLine(true);
+
+        // TODO : min, max 세팅 영양소별로 다르게 해줘야함
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setAxisMaximum(1000f);
+
+        YAxis rightAxis = holder.chart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+
+        // 그래프 그리기
+        CombinedData data = new CombinedData();
+        data.setData(generateBarData(totNutrition));
+        data.setData(generateScatterData(recNutrition));
+
+        holder.chart.setData(data);
+        holder.chart.invalidate();
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return nutritions.length;
+    }
+
+    // BarData : 실제 섭취량
+    private BarData generateBarData(float totNutrition) {
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        // TODO : 실제 섭취량 받아와서 넣기
+        entries.add(new BarEntry(0f, totNutrition));
+
+        BarDataSet set = new BarDataSet(entries, "실제 섭취량");
+        set.setColor(0xFFC9B3FF);
+        set.setValueTextColor(Color.BLACK);
+        set.setValueTextSize(10f);
+
+
+        BarData d = new BarData(set);
+        d.setBarWidth(0.3f);
+
+        return d;
+    }
+    // ScatterData : 권장 섭취량
+    private ScatterData generateScatterData(float recNutrition) {
+
+        ScatterData d = new ScatterData();
+
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        // TODO : 권장 섭취량 받아와서 넣기
+        entries.add(new Entry(0f, recNutrition));
+
+
+        ScatterDataSet set = new ScatterDataSet(entries, "권장 섭취량");
+        set.setColors(0xFF80FFFF);
+        set.setScatterShapeSize(20f);
+        set.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
+        set.setDrawValues(false);
+        set.setValueTextSize(10f);
+
+        d.addDataSet(set);
+
+        return d;
+    }
+}
