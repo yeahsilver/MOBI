f5344f0141da79252ca4ab4146b05fd9bfb34af4
seungjikim <43927910+seungjikim@users.noreply.github.com>
2020-08-04 03:15:08 +0900

    add user check dialog adding daily intake

---
+++ app/src/main/java/com/mp/test_cv/CheckDialog.java
@@ -0,0 +1,54 @@
+package com.mp.test_cv;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class CheckDialog extends DialogFragment {
+    public interface CheckDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    CheckDialogListener listener;
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (CheckDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException("CheckDialog"
+                    + " must implement NoticeDialogListener");
+        }
+    }
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_message)
+                .setTitle(R.string.dialog_title)
+                .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                        listener.onDialogPositiveClick(CheckDialog.this);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        listener.onDialogNegativeClick(CheckDialog.this);
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
--- app/src/main/java/com/mp/test_cv/NutritionInfoActivity.java
+++ app/src/main/java/com/mp/test_cv/NutritionInfoActivity.java
@@ -1,5 +1,6 @@
 package com.mp.test_cv;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,7 @@ import java.util.Map;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -30,7 +32,7 @@ import com.google.firebase.firestore.FieldValue;
 
 import org.opencv.android.CameraActivity;
 
-public class NutritionInfoActivity extends AppCompatActivity {
+public class NutritionInfoActivity extends AppCompatActivity implements CheckDialog.CheckDialogListener  {
     final String TAG = getClass().getSimpleName();
     PersonalDailyIntake inputIntake;
     TotalDailyIntake inputTotal;
@@ -41,6 +43,7 @@ public class NutritionInfoActivity extends AppCompatActivity {
     String date;
     boolean getUser = false;
     boolean getDocument = false;
+    boolean dialogCheck = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,117 +91,17 @@ public class NutritionInfoActivity extends AppCompatActivity {
                     myStartActivity(CameraView.class);
                     break;
                 case R.id.submit:
-                    calories = Integer.valueOf(((EditText)findViewById(R.id.editCalorie)).getText().toString());
-                    carbohydrate = Integer.valueOf(((EditText)findViewById(R.id.editCarbo)).getText().toString());
-                    protein = Integer.valueOf(((EditText)findViewById(R.id.editProtein)).getText().toString());
-                    fat = Integer.valueOf(((EditText)findViewById(R.id.editFat)).getText().toString());
-                    saturatedFat = Integer.valueOf(((EditText)findViewById(R.id.editSaturFat)).getText().toString());
-                    sugar = Integer.valueOf(((EditText)findViewById(R.id.editSugar)).getText().toString());
-                    sodium = Integer.valueOf(((EditText)findViewById(R.id.editSodium)).getText().toString());
-                    dietaryFiber = Integer.valueOf(((EditText)findViewById(R.id.editFiber)).getText().toString());
-                    inputIntake = new PersonalDailyIntake(calories, carbohydrate, protein, fat, saturatedFat, sugar, sodium, dietaryFiber);
-                    inputTotal = new TotalDailyIntake(calories, carbohydrate, protein, fat, saturatedFat, sugar, sodium, dietaryFiber);
-                    //사용자 정보 db 받고,inputIntake로 해당 정보들 갱신
-                    //모든 정보 갱신 과정이 끝나면 mainActivity로 간다. (ui업데이트)
-                    if (carbohydrate >= 0 && protein >= 0 && fat >= 0 && saturatedFat >= 0 && sugar >= 0 && sodium >= 0 && dietaryFiber >= 0 ) {
-                        user = FirebaseAuth.getInstance().getCurrentUser();
-                        db = FirebaseFirestore.getInstance();
-
-                        Date today = new Date();
-                        SimpleDateFormat timeFormat = new SimpleDateFormat("yyyyMMddhhmmss");
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-                        String time = timeFormat.format(today);
-                        date = dateFormat.format(today);
-                        DocumentReference totalDB = db.collection("TotalDailyIntake").document(date);
-                        Log.d(TAG, "totalDB error : "+totalDB.equals(date));
-
-                        if (user != null){
-                            // DailyIntake 생성
-                            db.collection("User").document(user.getUid())
-                                    .collection("DailyIntake").document(time)
-                                    .set(inputIntake)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void aVoid) {
-                                            startToast("정보 입력에 성공했습니다..");
-                                            getUser = true;
-                                            finish();
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            startToast("정보 입력에 실패했습니다..");
-                                            Log.w(TAG, "Error writing document", e);
-                                        }
-                                    });
-
-                            DocumentReference docRef = db.collection("User").document(user.getUid()).collection("TotalDailyIntake").document(date);
-                            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                    if (task.isSuccessful()) {
-                                        DocumentSnapshot document = task.getResult();
-                                        Log.d(TAG, "Document: " + document);
-                                        docRefMap = document.getData();
-                                        if (document.exists()) {
-                                            Log.d(TAG, "docRefMap: " + docRefMap);
-
-                                        } else {
-                                            Log.d(TAG, "docRefMap: " + docRefMap);
-                                            Log.d(TAG, "No such document");
-                                        }
-                                    } else {
-                                        Log.d(TAG, "get failed with ", task.getException());
-                                    }
-                                    // TotalDailyIntake 생성
-                                    if(docRefMap == null){
-                                        db.collection("User").document(user.getUid())
-                                                .collection("TotalDailyIntake").document(date)
-                                                .set(inputTotal)
-                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                    @Override
-                                                    public void onSuccess(Void aVoid) {
-                                                        startToast("정보를 초기화했습니다.");
-                                                        getDocument = true;
-                                                        finish();
-                                                    }
-                                                })
-                                                .addOnFailureListener(new OnFailureListener() {
-                                                    @Override
-                                                    public void onFailure(@NonNull Exception e) {
-                                                        startToast("정보 초기화를 실패했습니다.");
-                                                        Log.w(TAG, "Error writing document", e);
-                                                    }
-                                                });
-                                    }else{
-                                        // TotalDailyIntake Increment
-                                        DocumentReference tdb = db.collection("User").document(user.getUid())
-                                                .collection("TotalDailyIntake").document(date);
-                                        tdb.update("totalCalories", FieldValue.increment(calories));
-                                        tdb.update("totalCarbohydrate", FieldValue.increment(carbohydrate));
-                                        tdb.update("totalDietaryFiber", FieldValue.increment(dietaryFiber));
-                                        tdb.update("totalFat", FieldValue.increment(protein));
-                                        tdb.update("totalProtein", FieldValue.increment(fat));
-                                        tdb.update("totalSaturatedFat", FieldValue.increment(saturatedFat));
-                                        tdb.update("totalSodium", FieldValue.increment(sugar));
-                                        tdb.update("totalSugar", FieldValue.increment(sodium));
-                                        getDocument = true;
-                                    }
-                                }
-                            });
-                        }
-                    }
-                    else {
-                        startToast("사용자정보를 입력하세요.");
-                    }
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class );
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    //로그인 한 상태에서 뒤로가기 눌렀을 때 메인액티비로 이동, 나머지 스택 없어짐.
-                    startActivity(intent);
+                    showCheckDialog();
+                    break;
             }
         }
     };
+   /* private void setUpDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_message)
+                .setTitle(R.string.dialog_title);
+        AlertDialog dialog = builder.create();
+    }*/
     private void startToast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
@@ -207,5 +110,132 @@ public class NutritionInfoActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         //로그인 한 상태에서 뒤로가기 눌렀을 때 메인액티비로 이동, 나머지 스택 없어짐.
         startActivity(intent);
+    }
+    public void showCheckDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new CheckDialog();
+        dialog.show(getSupportFragmentManager(), "NoticeDialogFragment");
+    }
+    public void sendData() {
+        if(dialogCheck) {
+            calories = Integer.valueOf(((EditText) findViewById(R.id.editCalorie)).getText().toString());
+            carbohydrate = Integer.valueOf(((EditText) findViewById(R.id.editCarbo)).getText().toString());
+            protein = Integer.valueOf(((EditText) findViewById(R.id.editProtein)).getText().toString());
+            fat = Integer.valueOf(((EditText) findViewById(R.id.editFat)).getText().toString());
+            saturatedFat = Integer.valueOf(((EditText) findViewById(R.id.editSaturFat)).getText().toString());
+            sugar = Integer.valueOf(((EditText) findViewById(R.id.editSugar)).getText().toString());
+            sodium = Integer.valueOf(((EditText) findViewById(R.id.editSodium)).getText().toString());
+            dietaryFiber = Integer.valueOf(((EditText) findViewById(R.id.editFiber)).getText().toString());
+            inputIntake = new PersonalDailyIntake(calories, carbohydrate, protein, fat, saturatedFat, sugar, sodium, dietaryFiber);
+            inputTotal = new TotalDailyIntake(calories, carbohydrate, protein, fat, saturatedFat, sugar, sodium, dietaryFiber);
+            //사용자 정보 db 받고,inputIntake로 해당 정보들 갱신
+            //모든 정보 갱신 과정이 끝나면 mainActivity로 간다. (ui업데이트)
+            if (carbohydrate >= 0 && protein >= 0 && fat >= 0 && saturatedFat >= 0 && sugar >= 0 && sodium >= 0 && dietaryFiber >= 0) {
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                db = FirebaseFirestore.getInstance();
+
+                Date today = new Date();
+                SimpleDateFormat timeFormat = new SimpleDateFormat("yyyyMMddhhmmss");
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+                String time = timeFormat.format(today);
+                date = dateFormat.format(today);
+                DocumentReference totalDB = db.collection("TotalDailyIntake").document(date);
+                Log.d(TAG, "totalDB error : " + totalDB.equals(date));
+
+                if (user != null) {
+                    // DailyIntake 생성
+                    db.collection("User").document(user.getUid())
+                            .collection("DailyIntake").document(time)
+                            .set(inputIntake)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    startToast("정보 입력에 성공했습니다..");
+                                    getUser = true;
+                                    finish();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    startToast("정보 입력에 실패했습니다..");
+                                    Log.w(TAG, "Error writing document", e);
+                                }
+                            });
+
+                    DocumentReference docRef = db.collection("User").document(user.getUid()).collection("TotalDailyIntake").document(date);
+                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                Log.d(TAG, "Document: " + document);
+                                docRefMap = document.getData();
+                                if (document.exists()) {
+                                    Log.d(TAG, "docRefMap: " + docRefMap);
+
+                                } else {
+                                    Log.d(TAG, "docRefMap: " + docRefMap);
+                                    Log.d(TAG, "No such document");
+                                }
+                            } else {
+                                Log.d(TAG, "get failed with ", task.getException());
+                            }
+                            // TotalDailyIntake 생성
+                            if (docRefMap == null) {
+                                db.collection("User").document(user.getUid())
+                                        .collection("TotalDailyIntake").document(date)
+                                        .set(inputTotal)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                startToast("정보를 초기화했습니다.");
+                                                getDocument = true;
+                                                finish();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                startToast("정보 초기화를 실패했습니다.");
+                                                Log.w(TAG, "Error writing document", e);
+                                            }
+                                        });
+                            } else {
+                                // TotalDailyIntake Increment
+                                DocumentReference tdb = db.collection("User").document(user.getUid())
+                                        .collection("TotalDailyIntake").document(date);
+                                tdb.update("totalCalories", FieldValue.increment(calories));
+                                tdb.update("totalCarbohydrate", FieldValue.increment(carbohydrate));
+                                tdb.update("totalDietaryFiber", FieldValue.increment(dietaryFiber));
+                                tdb.update("totalFat", FieldValue.increment(protein));
+                                tdb.update("totalProtein", FieldValue.increment(fat));
+                                tdb.update("totalSaturatedFat", FieldValue.increment(saturatedFat));
+                                tdb.update("totalSodium", FieldValue.increment(sugar));
+                                tdb.update("totalSugar", FieldValue.increment(sodium));
+                                getDocument = true;
+                            }
+                        }
+                    });
+                }
+            } else {
+                startToast("사용자정보를 입력하세요.");
+            }
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            //로그인 한 상태에서 뒤로가기 눌렀을 때 메인액티비로 이동, 나머지 스택 없어짐.
+            startActivity(intent);
+        }
+    }
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        dialogCheck = true;
+        sendData();
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        dialogCheck = true;
+        sendData();
     }
 }
